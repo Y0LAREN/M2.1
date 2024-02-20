d975ed2a51e7e3498398a91b7c59e680888d393d
Ivanov Ilya <gergen892@gmail.com>
2024-02-20 08:23:53 +0500

    initial

---
+++ M2.1/Resources.swift
@@ -0,0 +1,30 @@
+//
+//  Resources.swift
+//  M2.1
+//
+//  Created by Илья Иванов on 20.02.2024.
+//
+
+import Foundation
+import UIKit
+
+enum Constants{
+    
+    enum Colors{
+        static let colorBlack = UIColor.black
+        static let colorYellow = UIColor.yellow
+        static let colorGreen = UIColor.green
+    }
+    
+    enum Fonts{
+        static let font1 = UIFont.systemFont(ofSize: 14)
+        static let font2 = UIFont.systemFont(ofSize: 16)
+        static let font3 = UIFont.systemFont(ofSize: 24)
+    }
+    
+    enum Texts{
+        static let text1 = "Hello, World"
+        static let text2 = "I think that, look it the beatifull things"
+        static let text3 = " ^_^ "
+    }
+}
---
+++ M2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M2.1
+//
+//  Created by Илья Иванов on 20.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
